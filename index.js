c82576b2212a86d92f4227c54358645572623ad1
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2023-12-19 17:09:56 -0800

    some functionally added for btn

---
+++ index.js
@@ -0,0 +1,14 @@
+const contentSection = document.getElementById("content-section")
+const searchBtn = document.getElementById("search-btn")
+async function getMoviesHTML(){
+    const res = await fetch("http://www.omdbapi.com/?apikey=acec20f1&t=blade+runner", {method:"GET"})
+    const data = await res.json()
+    console.log(data)
+}
+getMoviesHTML()
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault()
+    const inputText = document.getElementById("search-input").value
+    console.log(inputText)
+    console.log("clicked")
+})
