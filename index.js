32b72f35cd92cc1a8d3c49ed9f26cc2af797d6ae
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2023-12-20 15:11:36 -0800

    html css first commit

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const contentSection = document.getElementById("content-section")
-const searchBtn = document.getElementById("search-btn")
-async function getMoviesHTML(){
-    let moviesHTML = ''
-    const res = await fetch("http://www.omdbapi.com/?apikey=acec20f1&t=blade+runner", {method:"GET"})
-    const data = await res.json()
-    console.log(data)
-}
-getMoviesHTML()
-searchBtn.addEventListener("click", function(e){
-    e.preventDefault()
-    const inputText = document.getElementById("search-input").value
-    console.log(inputText)
-    console.log("clicked")
-})
+if (localStorage.getItem("watchlist") === null) {
+    localStorage.setItem("watchlist", JSON.stringify([]));
+  }
+  
+const contentSection = document.getElementById("content-section");
+const inputText = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+
+let page = 1;
+let searchedtitle = "";
+let pageStorage = [];
+
+// async function searchMovie(){
+//     const res = await fetch(`http://www.omdbapi.com/?apikey=acec20f1&s=${inputText.value}`, {method:"GET"});
+//     const data = await res.json();
+//     console.log(data);
+// }
+// searchBtn.addEventListener("click", function(e){
+//     e.preventDefault();
+//     searchMovie();
+// })
+const getImdbIds = async (movieTitle, page) => {
+    contentSection.innerHTML = `<div class="loader"></div>`
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=3dce0b9&s=${movieTitle}&page=${page}&type=movie`
+    );
+    const data = await response.json();
+  
+    if (data.Search) {
+      
+      return data.Search.map((movie) => movie.imdbID);
+    } 
+  };
+  console.log(getImdbIds("blade runner", 1))
