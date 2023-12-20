e831d7b62fb131d9df952d73bf92e4246bfb2313
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2023-12-19 20:43:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const contentSection = document.getElementById("content-section")
 const searchBtn = document.getElementById("search-btn")
 async function getMoviesHTML(){
+    let moviesHTML = ''
     const res = await fetch("http://www.omdbapi.com/?apikey=acec20f1&t=blade+runner", {method:"GET"})
     const data = await res.json()
     console.log(data)
