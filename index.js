2ce57be0e7a9daa30ef4a2157f21aad3ab76a9fe
Sammy005 <98131603+Ezimy@users.noreply.github.com>
2023-12-20 20:52:27 -0800

    more js functionality added

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
 if (localStorage.getItem("watchlist") === null) {
     localStorage.setItem("watchlist", JSON.stringify([]));
   }
-  
+
 const contentSection = document.getElementById("content-section");
 const inputText = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
+const pageSection = document.getElementById("page-btn-section");
 
 let page = 1;
 let searchedtitle = "";
 let pageStorage = [];
 
-// async function searchMovie(){
-//     const res = await fetch(`http://www.omdbapi.com/?apikey=acec20f1&s=${inputText.value}`, {method:"GET"});
-//     const data = await res.json();
-//     console.log(data);
-// }
-// searchBtn.addEventListener("click", function(e){
-//     e.preventDefault();
-//     searchMovie();
-// })
-const getImdbIds = async (movieTitle, page) => {
-    contentSection.innerHTML = `<div class="loader"></div>`
-    const response = await fetch(
-      `https://www.omdbapi.com/?apikey=3dce0b9&s=${movieTitle}&page=${page}&type=movie`
-    );
-    const data = await response.json();
-  
-    if (data.Search) {
-      
-      return data.Search.map((movie) => movie.imdbID);
-    } 
-  };
-  console.log(getImdbIds("blade runner", 1))
+getPageHTML()
+
+function getPageHTML(){
+    if (page === 1){
+        pageSection.innerHTML = `
+        <button id="next-page" class=page-btn>Next Page</button>`
+    }
+    else{
+        pageSection.innerHTML = `
+        <button id="next-page" class=page-btn>Next Page</button>
+        <button id="previous-page"class=page-btn>Previous Page</button>`
+    }
+}
+
+async function getMoviesHTML(){
+    let moviesHTML = ''
+    const res = await fetch(`http://www.omdbapi.com/?apikey=acec20f1&s=${inputText.value}&page=${page}&type=movie`, {method:"GET"});
+    const data = await res.json();
+    for(let i =0 ; i < data.Search.length; i++){
+        moviesHTML += `<h1>${data.Search[i].Title}</h1>`
+    }
+    return moviesHTML
+}
+function render() {
+    getMoviesHTML().then(html => {
+        contentSection.innerHTML = html;
+    });
+}
+document.addEventListener('click', function(e){
+    if(e.target.id === "search-btn"){
+        e.preventDefault()
+        render()
+    }
+    if(e.target.id ==="next-page"){
+        page = page + 1
+        getPageHTML()
+        getMoviesHTML()
+    }
+    if(e.target.id ==="previous-page" && page != 1){
+        page = page - 1
+        getPageHTML()
+        getMoviesHTML()
+    }
+});
